a9e8e7d3c41078d32d2e22b7b7c271ce59e0d411
yeobi <kimhyjjang@gmail.com>
2026-01-05 23:40:52 +0900

    feat: create bean

---
+++ src/main/java/org/example/ProjectConfig.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Parrot parrot1() {
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    @Primary
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    @Bean
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName("Riki");
+        return p;
+    }
+}
