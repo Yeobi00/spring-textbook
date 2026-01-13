0ef8854bd8e91a4903b16f534bb89e5f9baab801
yeobi <kimhyjjang@gmail.com>
2026-01-13 15:40:08 +0900

    feat: value injection using class field with @Autowired

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,13 @@ package org.example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.function.Supplier;
-
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Person person = context.getBean(Person.class);
-        Parrot parrot = context.getBean(Parrot.class);
+        Person p = context.getBean(Person.class);
 
-        System.out.println("Person's name: " + person.getName());
-        System.out.println("Parrot's name: " + parrot.getName());
-        System.out.println("Person's parrot: " + person.getParrot());
+        System.out.println(p.getName());
+        System.out.println(p.getParrot());
     }
 }
